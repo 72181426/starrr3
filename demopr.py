df05a895586b0b03d5444398bbe550062a299d6b
Hugo <hugorodrigo9518@gmail.com>
2024-10-22 13:19:51 -0500

    commit2

--- demopr.py
+++ demopr.py
@@ -1,10 +1,7 @@
 c = [1, 2, 3, 4]
 for x in c:
-    y=x+1
+    y = x + 1
     print(y)
-print('a')
+print("a")
 print("asas")
-print('as')
-for m in c:
-    if m == 2:
-        print(m)
+print("as")
